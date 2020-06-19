9270908ba90dc41be391f5b9ea9296cf7e4e7c50
hsiaofongw <hsiaofong.w@gmail.com>
2020-06-19 16:31:10 +0800

    update

--- collectors/heartbeatsDataCollectorServer.ts
+++ collectors/heartbeatsDataCollectorServer.ts
@@ -3,37 +3,22 @@ const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 import { Readable } from "stream";
 import { DataManagementSystem } from "../analytics/dataManagementSystem";
+import crypto = require('crypto');
 
-export class HeartbeatsDataCollectorServer {
+interface AllocatableServer {
+    getName(): string;
 
-    private app: any;
-    private resourcesDeclaration: any;
-    private logStream: Readable;
-    private connected = false;
+    start(): Promise<boolean>;
+
+    getRoutesTable(): Object;
+}
+
+export class CollectorServer {
+    public connected = false;
+    public logStream: Readable;
 
     constructor() {
         this.initialize();
-    }
-
-    private async connectToStorage() {
-        let dms = new DataManagementSystem();
-        let logStorageEntryStream = await dms.getWritableStream(this.getName());
-
-        this.logStream.pipe(logStorageEntryStream);
-    }
-
-    public async start() {
-        await this.listen();
-        this.onServerStarted(this.getSocketName());
-        return true;
-    }
-
-    private onServerStarted(path: string) {
-        console.log(`Server ${this.getName()} started at ${path}`);
-    }
-
-    public getName() {
-        return "heartbeats";
     }
 
     private async initialize() {
@@ -42,14 +27,51 @@ export class HeartbeatsDataCollectorServer {
         });
     }
 
-    private getSocketName() {
+    public async appendLog(logObject) {
+        if (! this.connected) {
+            await this.connectToStorage();
+        }
+
+        this.logStream.push(logObject);
+    }
+
+    public getName() {
+        return "unnamedServer";
+    }
+
+    private async connectToStorage() {
+        let dms = new DataManagementSystem();
+        let logStorageEntryStream = await dms.getWritableStream(this.getName());
+
+        this.logStream.pipe(logStorageEntryStream);
+    }
+}
+
+export class HeartbeatsDataCollectorServer extends CollectorServer implements AllocatableServer {
+
+    private resourcesDeclaration: any;
+
+    public async start() {
+        await this.listen();
+        this.onServerStarted(this.getSocketName());
+        return true;
+    }
+
+    private onServerStarted(path: string): void {
+        console.log(`Server ${this.getName()} started at ${path}`);
+    }
+
+    public getName(): string {
+        return "heartbeats";
+    }
+
+    public getSocketName(): string {
         return __dirname + "/sockets/" + this.getName() + ".socket";
     }
 
     private listen() {
         const app = express();
         this.registerRoutes(app);
-        this.app = app;
 
         return new Promise((resolve, reject) => {
 
@@ -64,7 +86,7 @@ export class HeartbeatsDataCollectorServer {
         });
     }
 
-    public getRoutesTable() {
+    public getRoutesTable(): Object {
         return {
             "from": this.resourcesDeclaration,
             "to": this.getSocketName()
@@ -108,15 +130,154 @@ export class HeartbeatsDataCollectorServer {
         this.appendLog(newHeartbeat);
         res.json(newHeartbeat);
     }
+}
 
-    public async appendLog(logObject) {
-        if (! this.connected) {
-            await this.connectToStorage();
+export class IdentitiesLogsDataCollectorServer extends CollectorServer {
+
+    private masterSecret: string;
+    private logs = [];
+    private resourceDeclaration: any;
+    private app: any;
+
+    private getMasterSecret(): string {
+        return '';
+    }
+
+    public getName() {
+        return "identities";
+    }
+
+    public getRoutesTable() {
+        return {
+            "from": this.resourceDeclaration,
+            "to": this.getSocketName()
+        };
+    }
+
+    public getSocketName() {
+        return __dirname + "/sockets/" + this.getName() + ".socket";
+    }
+
+    private registerRoutes() {
+        const app = express();
+
+        app.use(
+            bodyParser.json({
+                type: "application/json"
+            })
+        );
+
+        app.use(
+            (err, req, res, next) => this.errorHandler(err, req, res, next)
+        );
+
+        this.resourceDeclaration = [
+            '/identitiesLogs',
+            '/identities'
+        ];
+
+        app.post('/identities', (req, res) => this.onIdentitiesRequest(req, res));
+
+        this.app = app;
+    }
+
+    public listen(path: number | string) {
+        console.log(`listen ${path}`);
+        this.app.listen(path, () => this.onServerStarted(path));
+    }
+
+    private errorHandler(err, req, res, next) {
+        console.log(err.message);
+        res.status(400);
+        res.json({
+            'message': err.message,
+            'ok': false
+        });
+    }
+
+    private onIdentitiesRequest(req, res) {
+        let purpose = req.body.purpose;
+        let result = true;
+        let identity = {};
+
+        if (purpose === 'issueNew') {
+            identity = this.makeUUIDObject();
+            res.json(identity);
+
+            result = true;
         }
 
-        this.logStream.push(logObject);
+        let datetime = Date.now();
+        let ipAddr = this.getIPAddress(req);
+        let userAgent = this.getUserAgent(req);
+        let origin = this.getOrigin(req);
+
+        this.logs.push({
+            "purpose": purpose,
+            "datetime": datetime,
+            "identity": identity,
+            "passed": result,
+            "serialNumber": uuidv4(),
+            "ipAddr": ipAddr,
+            "userAgent": userAgent,
+            "origin": origin
+        });
     }
+    
+    private makeUUIDObject() {
+        const uuidString = uuidv4();
+        const uuidHash = crypto.createHmac('sha512', this.getMasterSecret())
+            .update(uuidString)
+            .digest('hex');
+        
+        let uuidObject =  {
+            uuid: uuidString,
+            checkSum: uuidHash
+        };
+        
+        return uuidObject;
+    }
+    
+    public verifyUUIDObject(uuidObject) {
+        const receivedUUIDHash = uuidObject.checkSum;
+        const receivedUUIDString = uuidObject.uuid;
+        const computedUUIDHash = crypto.createHmac('sha512', this.getMasterSecret())
+            .update(receivedUUIDString)
+            .digest('hex');
+        
+        let checkResult = computedUUIDHash === receivedUUIDHash;
+    
+        return checkResult;
+    }
+    
+    private onServerStarted(path: number | string) {
+        console.log(`Server started at ${path}`);
+    }
+
+    private getUserAgent(req) {
+        let ua = req.headers['User-Agent'] || 
+            req.headers['user-agent'] ||
+            '';
+        return ua;
+    }
+
+    private getIPAddress(req) {
+        let ip = req.headers['x-forwarded-for'] ||
+            req.headers['X-Forwarded-For'] ||
+            req.connection.remoteAddress;
+    
+        return ip;
+    }
+
+    private getOrigin(req) {
+        let origin = req.headers['origin'] ||
+            req.headers['Origin'] || '';
+        
+        return origin;
+    }
+
 }
+
 
 // let heartbeats = new HeartbeatsDataCollectorServer();
 
